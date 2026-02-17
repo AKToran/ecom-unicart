1aa03a1e1502634e2938ea180f47f31c28805884
AKToran <aktoran60@gmail.com>
2026-02-17 12:47:30 +0600

    dynamically load categories

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
-console.log("hello...");
+const loadCategories = () =>{
+  fetch("https://fakestoreapi.com/products/categories")
+  .then(res => res.json())
+  .then(data => displayCategories(data))
+}
+
+const displayCategories = (categories) =>{
+  const categoryContainer = document.getElementById("category-container");
+
+  categories.forEach(category =>{
+    categoryContainer.innerHTML+= `<button class="btn btn-outline btn-primary">${category[0].toUpperCase()+category.slice(1)}</button>`
+  })
+}
+
+
+loadCategories();
